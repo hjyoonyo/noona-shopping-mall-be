ecd0c219a42463ad633d55418ac1a0ae5aadc0ed
hjyoonyo <hjyoon2048@gmail.com>
2024-10-25 23:29:53 +0900

    쇼핑몰 만들기 5~6

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ const mongoose = require('mongoose'); //DB
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
+const indexRouter = require("./routes/index");
+
 
 require("dotenv").config()
 app.use(cors());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json()) //req.body가 객체로 인식
 
-const mongoURI = process.env.LOCAL_DB_ADRESS;
+app.use("/api",indexRouter); // /api/user 하면 index.js로 갔다가 user.api로 감
+
+const mongoURI = process.env.LOCAL_DB_ADRESS || process.env.MONGODB_URI_PROD;
 mongoose.connect(mongoURI,{useNewUrlParser:true}).then(()=>console.log("mongoose connected")).catch((err)=>console.log("DB connection fail"));
 
 app.listen(process.env.PORT || 5000,()=>{
