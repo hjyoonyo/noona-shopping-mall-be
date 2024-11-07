9680c796c44899c0e8ae15c63d3bb1b259698e68
hjyoonyo <hjyoon2048@gmail.com>
2024-11-08 00:49:23 +0900

    쇼핑몰 만들기 28~29

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -1,8 +1,10 @@
+const { response } = require("express");
 const Order = require("../models/Order");
 const randomStringGenerator = require("../utils/randomStringGenerator");
 const productController = require("./product.controller");
 
 const orderController ={};
+const PAGE_SIZE = 3;
 
 orderController.createOrder=async(req,res)=>{
     try {
@@ -33,6 +35,77 @@ orderController.createOrder=async(req,res)=>{
     } catch (error) {
         res.status(400).json({status:"fail",error:error.message});
     }
+}
+
+orderController.getOrder = async(req,res)=>{
+    try {
+        const {userId} = req;
+        console.log("userId ",userId);
+        const orderList = await Order.find({userId})
+            .sort({ createdAt: -1 })
+            .populate({
+                path:'items',
+                populate:{
+                    path:'productId',
+                    model: "Product",
+                }
+            });
+        
+        
+        res.status(200).json({status:"success",orderList:orderList});
+    } catch (error) {
+        res.status(400).json({status:"fail",error:error.message});
+    }
+}
+
+orderController.getOrderList = async (req,res)=>{
+    try {
+        const { page, ordernum } = req.query;
+    
+        let cond = {};
+        if (ordernum) {
+          cond = {
+            orderNum: { $regex: ordernum, $options: "i" },
+          };
+        }
+    
+        const orderList = await Order.find(cond)
+          .sort({ createdAt: -1 })
+          .populate("userId")
+          .populate({
+            path: "items",
+            populate: {
+              path: "productId",
+              model: "Product",
+              select: "image name",
+            },
+          })
+          .skip((page - 1) * PAGE_SIZE)
+          .limit(PAGE_SIZE);
+        const totalItemNum = await Order.countDocuments(cond);
+    
+        const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
+        res.status(200).json({ status: "success", data: orderList, totalPageNum });
+    } catch (error) {
+        return res.status(400).json({ status: "fail", error: error.message });
+    }
+}
+
+orderController.updateOrder = async (req,res)=>{
+    try {
+        const { id } = req.params;
+        const { status } = req.body;
+        const order = await Order.findByIdAndUpdate(
+          id,
+          { status: status },
+          { new: true }
+        );
+        if (!order) throw new Error("해당 주문 내역을 찾을 수 없습니다.");
+    
+        res.status(200).json({ status: "success", data: order });
+      } catch (error) {
+        return res.status(400).json({ status: "fail", error: error.message });
+      }
 }
 
 module.exports=orderController;
--- routes/order.api.js
+++ routes/order.api.js
@@ -4,5 +4,13 @@ const authController = require("../controllers/auth.controller");
 const orderController = require("../controllers/order.controller");
 
 router.post("/",authController.authenticate,orderController.createOrder);
+router.get("/me",authController.authenticate,orderController.getOrder);
+router.get("/",authController.authenticate,orderController.getOrderList);
+router.put(
+    "/:id",
+    authController.authenticate,
+    authController.checkAdminPermission,
+    orderController.updateOrder
+)
 
 module.exports=router;
